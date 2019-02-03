7af457d3b14f68e6d32d6e820ee7fac2d4825762
Mauro Levra <mauro@nazardo.it>
2019-02-03 19:00:24 +0100

    Update with new state machine

--- src/main.cpp
+++ src/main.cpp
@@ -10,10 +10,6 @@ const uint8_t netmask[] = {255, 255, 255, 0};
 const uint16_t udpPort = 10089;
 const uint8_t serverIp[] = {192, 168, 89, 10};
 
-bool bootSequence_YELLOW = false;
-bool systemActive_GREEN = false;
-bool shutdownSequence_RED = false;
-
 const uint8_t InternalStatus_Idle = 0;
 const uint8_t InternalStatus_SendStart = 1;
 const uint8_t InternalStatus_SendStop = 2;
@@ -26,20 +22,37 @@ uint8_t remoteStatus = RemoteStatus_StoppingOrOff;
 unsigned long lastSendMillis = 0;
 const unsigned long SendIntervalMilliseconds = 1000;
 
+unsigned long blinkMillis = 0;
+const unsigned long BlinkIntervalMilliseconds = 500;
+bool blinkMultiplier = false;
+
 // Start/Stop message: [1 : Type(1B)][1 : Version(1B)][0/1 : Command(1B)][0..3 : HWConfig(1B)]
 char startStopMessage[] = {0x01, 0x01, 0x00, 0x00};
 
 void onUdpPacketReceived(uint16_t, uint8_t *, uint16_t, const char *, uint16_t);
 
+const uint8_t LedRed = PIN2;
+const uint8_t LedYellow = PIN3;
+const uint8_t LedGreen = PIN4;
+const uint8_t ButtonStart = PIN5;
+const uint8_t ButtonStop = PIN6;
+
+bool ledRedActive = false;
+bool ledRedBlinking = false;
+bool ledYellowActive = false;
+bool ledYellowBlinking = true;
+bool ledGreenActive = false;
+bool ledGreenBlinking = false;
+
 void setup()
 {
   Serial.begin(9600);
   wdt_enable(WDTO_8S);
-  pinMode(PIN2, OUTPUT); // bootSequence_YELLOW
-  pinMode(PIN3, OUTPUT); // systemActive_GREEN
-  pinMode(PIN4, OUTPUT); // shutdownSequence_RED
-  pinMode(PIN5, INPUT);  // START button
-  pinMode(PIN6, INPUT);  // STOP button
+  pinMode(LedRed, OUTPUT);
+  pinMode(LedYellow, OUTPUT);
+  pinMode(LedGreen, OUTPUT);
+  pinMode(ButtonStart, INPUT);
+  pinMode(ButtonStop, INPUT);
   ether.begin(sizeof Ethernet::buffer, macaddr, SS);
   ether.staticSetup(ip, 0, 0, netmask);
   ether.copyIp(ether.hisip, serverIp);
@@ -53,20 +66,24 @@ void loop()
   wdt_reset();
   uint16_t recv = ether.packetReceive();
   ether.packetLoop(recv);
-  digitalWrite(PIN2, bootSequence_YELLOW);
-  digitalWrite(PIN3, systemActive_GREEN);
-  digitalWrite(PIN4, shutdownSequence_RED);
-  if (digitalRead(PIN5) && remoteStatus == RemoteStatus_StoppingOrOff)
+  digitalWrite(LedRed, ledRedActive || (ledRedBlinking && blinkMultiplier));
+  digitalWrite(LedYellow, ledYellowActive || (ledYellowBlinking && blinkMultiplier));
+  digitalWrite(LedGreen, ledGreenActive || (ledGreenBlinking && blinkMultiplier));
+  if (digitalRead(ButtonStart) && remoteStatus == RemoteStatus_StoppingOrOff)
   {
     internalStatus = InternalStatus_SendStart;
   }
-  else if (digitalRead(PIN6) && remoteStatus == RemoteStatus_StartingOrOn)
+  else if (digitalRead(ButtonStop) && remoteStatus == RemoteStatus_StartingOrOn)
   {
     internalStatus = InternalStatus_SendStop;
   }
+  unsigned long now = millis();
+  if (now - blinkMillis > BlinkIntervalMilliseconds)
+  {
+    blinkMultiplier = !blinkMultiplier;
+  }
   if (internalStatus == InternalStatus_SendStart || internalStatus == InternalStatus_SendStop)
   {
-    unsigned long now = millis();
     if ((now - lastSendMillis) > SendIntervalMilliseconds)
     {
       startStopMessage[2] = internalStatus == InternalStatus_SendStart ? 1 : 0;
@@ -80,11 +97,15 @@ void onStatusMessageReceived(const char *data, uint16_t len, uint8_t version)
 {
   if (version == 1 && len == 3)
   {
-    uint8_t ledStatus = (uint8_t)data[2];
-    bootSequence_YELLOW = ledStatus & 0x01;
-    systemActive_GREEN = ledStatus & 0x02;
-    shutdownSequence_RED = ledStatus & 0x04;
-    if (bootSequence_YELLOW || (systemActive_GREEN && !shutdownSequence_RED))
+    uint8_t stateMachineStatus = (uint8_t)data[2];
+    ledYellowBlinking = false; // Stop blinking since message has arrived
+    ledRedActive = stateMachineStatus == 0 || stateMachineStatus == 6;
+    ledRedBlinking = stateMachineStatus == 5;
+    ledGreenActive = stateMachineStatus == 2 || stateMachineStatus == 3;
+    ledGreenBlinking = stateMachineStatus == 1;
+    ledYellowActive = !(stateMachineStatus == 0 || stateMachineStatus == 3);
+
+    if (stateMachineStatus > 0 && stateMachineStatus < 4)
     {
       remoteStatus = RemoteStatus_StartingOrOn;
       if (internalStatus == InternalStatus_SendStart)
